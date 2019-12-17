be7827a750ebab2e2547a2d4dcf1873e7ed562c1
Matt Cheah <matt.j.cheah@gmail.com>
2019-12-17 11:22:43 +1100

    Move NewGameLanding to its own file

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 // import Popup from "reactjs-popup";
-import { validateNewGame, validateJoinGame, getQuote } from "../Helpers";
+import { validateJoinGame, getQuote } from "../Helpers";
 import "./tooltips.scss";
 import ServerContext from "../ServerContext";
+import NewGameLanding from "./NewGameLanding";
 
 const RoomContext = React.createContext();
 
@@ -60,77 +61,6 @@ function InitialLanding({ setStage }) {
         </button>
         <button className="landingButton" onClick={() => setStage("joinGame")}>
           JOIN
-        </button>
-      </div>
-    </div>
-  );
-}
-
-function NewGameLanding({ setStage }) {
-  const [name, setName] = useState("");
-  const [gameLength, setGameLength] = useState("5");
-
-  const [isValid, validMessage] = validateNewGame(name, gameLength);
-
-  const server = useContext(ServerContext);
-
-  const handleChangeUserName = event => {
-    setName(event.target.value);
-  };
-
-  const handleChangeGameLength = event => {
-    setGameLength(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    const socket = server.socket;
-
-    socket.createRoom(name);
-  };
-
-  if (server.roomID) {
-    return <Redirect to={`/${server.roomID}`} />;
-  }
-
-  return (
-    <div>
-      <div className="menuBorderContainer">
-        <div className="inputFieldContainer">
-          <input
-            type="text"
-            name="user_name"
-            placeholder="Enter your name..."
-            value={name}
-            onChange={handleChangeUserName}
-            className="inputField"
-          />
-          <div style={{ margin: "20px auto" }}>
-            <div style={{ display: "inline-block" }}>
-              Game Length (minutes):
-            </div>
-            <input
-              style={{ display: "inline-block" }}
-              type="text"
-              name="user_name"
-              value={gameLength}
-              onChange={handleChangeGameLength}
-              className="inputField inputFieldNumber"
-            />
-          </div>
-        </div>
-        {isValid ? (
-          <button className="landingButton" onClick={handleSubmit}>
-            CREATE
-          </button>
-        ) : (
-          <span tooltip={validMessage} flow="left">
-            <button className=" disabledButton" disabled={true}>
-              CREATE
-            </button>
-          </span>
-        )}
-        <button className="landingButton" onClick={() => setStage("initial")}>
-          BACK
         </button>
       </div>
     </div>
