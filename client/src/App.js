6b80279adc5f405d4ff673dcd4d3cba30fccf36c
Matt Cheah <matt.j.cheah@gmail.com>
2020-09-13 18:10:17 +1000

    Remove socket-related code

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,20 @@
-import React, { useEffect } from "react";
-import Landing from "./screens/Landing";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import GameRoom from "./screens/GameRoom";
-import Socket from "./services/socket";
-import ServerContext from "./components/ServerContext";
-import { useServer } from "./components/useServer";
 import { ApolloProvider } from "@apollo/client";
+
 import client from "./graphql/apollo";
+import Landing from "./screens/Landing";
+import GameRoom from "./screens/GameRoom";
 
-function App() {
-  const [state, dispatch] = useServer();
-
-  useEffect(() => {
-    const socket = new Socket(dispatch);
-
-    dispatch({ type: "INIT_SOCKET", socket });
-
-    return () => {
-      socket.closeConnection();
-    };
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch({
-      type: "INIT_CLEAR_ERROR",
-      clearError: () => dispatch({ type: "SET_JOIN_ERROR", errorMessage: "" }),
-    });
-  }, [dispatch]);
-
+const App = () => {
   return (
     <ApolloProvider client={client}>
-      <ServerContext.Provider value={state}>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/:roomID" component={GameRoom} />
-        </Switch>
-      </ServerContext.Provider>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/:roomID" component={GameRoom} />
+      </Switch>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
