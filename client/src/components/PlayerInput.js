c42a560bcfc358eb1e77920582b8961844de542a
Matt Cheah <matt.j.cheah@gmail.com>
2020-09-17 20:54:00 +1000

    Convert PlayerInput to styled components

--- client/src/components/PlayerInput.js
+++ client/src/components/PlayerInput.js
@@ -1,4 +1,47 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  width: 50%;
+  margin: 0.25rem auto;
+  flex-direction: row;
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.2);
+
+  & > * {
+    font-size: 1.25rem;
+    font-weight: 900;
+  }
+`;
+
+const Input = styled.input`
+  font-weight: 900;
+
+  flex: 1;
+  padding: 0.6em;
+  border: 0;
+  border-bottom: 0.25rem solid transparent;
+
+  &:focus {
+    outline: none;
+    border-bottom: 0.25rem solid var(--darktan);
+  }
+`;
+
+const Button = styled.button`
+  padding: 0.6em 0.8em;
+  background-color: var(--darktan);
+  color: #fff;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 const PlayerInput = ({ onSubmit }) => {
   const [currentInput, setCurrentInput] = useState("");
@@ -12,17 +55,16 @@ const PlayerInput = ({ onSubmit }) => {
   };
 
   return (
-    <form className="field" onSubmit={handleSubmit}>
-      <input
+    <Form onSubmit={handleSubmit}>
+      <Input
         type="text"
-        id="searchterm"
         placeholder="Enter a Word..."
         value={currentInput}
         onChange={(e) => setCurrentInput(e.target.value)}
         autoComplete="off"
       />
-      <button type="submit">GO</button>
-    </form>
+      <Button type="submit">GO</Button>
+    </Form>
   );
 };
 
