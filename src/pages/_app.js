f62ff35d0a4623a70baa91d2d13ce0eeecfd5999
Matt Cheah <matt.j.cheah@gmail.com>
2022-10-07 11:43:31 +1100

    Basic Next.js setup

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import "../index.css";
+import "reactjs-popup/dist/index.css";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>testing</div>;
+};
+
+export default Home;
